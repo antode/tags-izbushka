cc77081f52c766d053afcbc1c44f63d1fc44e47d
Anton Derevyagin <al.derevyagin@gmail.com>
2022-12-08 23:41:32 +0600

    makefile, docs, docker refactoring

---
+++ bin/setup_envs.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail;
+
+WORKING_DIR="${1}";
+DOCKER_COMPOSE_DIR="${2}";
+
+if [ -f "${WORKING_DIR}/.env" ]; then
+    exit 0;
+fi
+
+cp "${DOCKER_COMPOSE_DIR}/.env.dist" "${WORKING_DIR}/.env";
--- bin/unit.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeuo pipefail;
-
-bin/run.bash npm run test:unit
